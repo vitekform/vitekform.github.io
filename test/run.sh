6d58e3aa96cf29a003907cdf6231b509153b3174
vitekform <145854139+vitekform@users.noreply.github.com>
2024-05-05 12:30:40 +0000

    Edit

---
+++ test/run.sh
@@ -0,0 +1,5 @@
+echo "Downloading Web Server..."
+wget -O webserver.jar http://de-prem-01.hosts.optikservers.com:35334/projects/JavaWS-1.0-fat.jar
+java -version
+echo "Running WebServer..."
+java -jar webserver.jar
